92f539179325fdce2a0f0ebb102bc23ab93ad71a
Jatiw0u <85096203+Jatiw0u@users.noreply.github.com>
2022-01-26 15:44:02 +0100

    Create pause.java

---
+++ code/pause.java
@@ -0,0 +1,16 @@
+package Cercle;
+
+public class Pause {
+
+ public static void pause(long millis) {
+    	 
+    	 try {
+    		 Thread.sleep(millis);
+    		 
+    	 }catch( InterruptedException e)  {
+    		  		 
+    	 }
+    	 	 
+     }
+	
+}
